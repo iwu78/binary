22e885132cd1649d1295871c869596b30552c6a8
iwu_78 <keledang777@gmail.com>
2023-11-15 20:07:35 -0800

    updated image decryption

--- assets/js/steganographyDecode.js
+++ assets/js/steganographyDecode.js
@@ -16,46 +16,31 @@ function handleImageUpload(input, callback) {
     };
     reader.readAsDataURL(file);
 }
-function handleImageUpload2(input, callback) {
-    const file = input.files[0];
-    const reader = new FileReader();
-    reader.onload = function (e) {
-        const image = new Image();
-        image.onload = function () {
-            ctx.canvas.width = image.width * 2;
-            ctx.canvas.height = image.height * 2;
-            ctx.drawImage(image, 0, 0, image.width * 2, image.height * 2);
-            const imageData = ctx.getImageData(0, 0, image.width * 2, image.height * 2);
-            callback(imageData);
-        };
-        image.src = e.target.result;
-    };
-    reader.readAsDataURL(file);
-}
 function processImages() {
-    handleImageUpload2(imageInput, function(imageData1) {
+    handleImageUpload(imageInput, function(imageData1) {
         var data1 = imageData1.data;
-        ctx.canvas.height = data1.height/2;
-        ctx.canvas.width = data1.width/2;
+        ctx.canvas.height = imageData1.height;
+        ctx.canvas.width = imageData1.width;
         var decryptedImageData = ctx.createImageData(200, 200);
         
         newdata = data1;
 
-        for (let i = 0; i < newdata.length; i += 4) {
-            const bits2 = [
-                (newdata[i] & 0x03) << 6,
-                (newdata[i + 1] & 0x03) << 4,
-                (newdata[i + 2] & 0x03) << 2,
-                newdata[i + 3] & 0x03
-            ];
-    
-            decryptedImageData.data[i] = (newdata[i] & 0xFC) | bits2[0];
-            decryptedImageData.data[i + 1] = (newdata[i + 1] & 0xFC) | bits2[1];
-            decryptedImageData.data[i + 2] = (newdata[i + 2] & 0xFC) | bits2[2];
-            decryptedImageData.data[i + 3] = (newdata[i + 3] & 0xFC) | bits2[3];
-        }
+        for (let i = 0; i < data1.length; i += 4) {
+            var pixelValue = (
+                ((data1[i] & 0x03) << 6) |
+                ((data1[i + 1] & 0x03) << 4) |
+                ((data1[i + 2] & 0x03) << 2) |
+                (data1[i + 3] & 0x03)
+            );
 
-        ctx.clearRect(0, 0, 200, 200);
+            decryptedImageData.data[i / 4] = pixelValue;
+            decryptedImageData.data[i / 4 + 1] = pixelValue;
+            decryptedImageData.data[i / 4 + 2] = pixelValue;
+            decryptedImageData.data[i / 4 + 3] = 255; // Alpha channel set to 255
+        }
+        console.log(newdata)
+
+        ctx.clearRect(0, 0, imageData1.width, imageData1.height);
         ctx.putImageData(decryptedImageData, 0, 0);
 
         const resultImage = new Image();
